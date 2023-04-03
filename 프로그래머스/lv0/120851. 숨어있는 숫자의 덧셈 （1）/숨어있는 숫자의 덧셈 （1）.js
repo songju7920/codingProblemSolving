d52fd4eaf609f3fa71cc32740989b2a4aa812e21
오송주 <99471963+songju7920@users.noreply.github.com>
2023-04-03 19:42:37 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -0,0 +1,7 @@
+function solution(my_string) {
+    var answer = 0;
+    for(let i = 0; i < my_string.length; i++)
+        if(isNaN(Number(my_string[i])) == false)
+            answer += Number(my_string[i]);
+    return answer;
+}
