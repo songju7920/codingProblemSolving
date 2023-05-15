9e39f1cb7f3d88379a427e36fc6ec5650b29bd72
오송주 <99471963+songju7920@users.noreply.github.com>
2023-05-15 19:25:17 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,9 +1,13 @@
 function solution(progresses, speeds) {
     let answer = [], results = [], rep = 0;
+    
+    //개발
     progresses.forEach(progress => {
         results.push(Math.ceil((100-progress)/speeds[rep]));
         rep++;
     })
+    
+    //배포
     while(results[0] != null){
         let Cnt = 1;
         let result = results.shift();
