3f280718c761e136172165b40a0aed3c42280250
오송주 <99471963+songju7920@users.noreply.github.com>
2023-05-22 18:42:00 +0900

    [level 2] Title: 프로세스, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.js
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.js
@@ -1,25 +1,19 @@
 function solution(priorities, location) {
   let answer = 0;
-  let target = priorities[location];
 
   while (true) {
     let max = Math.max(...priorities);
     let first = priorities.shift();
 
-    if (max === first) {
+    if (max == first) {
       answer++;
-      if (location === 0) {
-        break;
-      } else {
-        location--;
-      }
-    } else {
+      if (location == 0) break;
+      else location--;
+    } 
+    else {
       priorities.push(first);
-      if (location === 0) {
-        location = priorities.length - 1;
-      } else {
-        location--;
-      }
+      if (location == 0) location = priorities.length - 1;
+      else location--;
     }
   }
 
