3d93eb288f60e0c1ba09a87a824a768ce14b5ff2
오송주 <99471963+songju7920@users.noreply.github.com>
2023-05-26 10:14:42 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,5 @@
+function solution(phone_number) {
+    phone_number = phone_number.split('');
+    for(let i = 0; i < phone_number.length - 4; i++) phone_number[i] = '*'
+    return phone_number.reduce((a,b) => a+b);
+}
