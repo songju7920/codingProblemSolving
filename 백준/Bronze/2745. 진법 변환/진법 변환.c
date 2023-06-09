071d56457a2f9969a58acd6f49c1b00ee096d1bb
오송주 <99471963+songju7920@users.noreply.github.com>
2023-06-09 09:18:51 +0900

    [Bronze II] Title: 진법 변환, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.c
+++ 백준/Bronze/2745. 진법 변환/진법 변환.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
   int n, sum = 0, square = 1, length = -1;
-  char numbers[1000] = {-1};
+  char numbers[1000];
 
   //입력받기
   for (int i = 0;; i++) {
     scanf("%c", &numbers[i]);
-    if (numbers[i] == 32) {
-      numbers[i] = -1;
-      break;
-    }
-    if (numbers[i] > 64)
-      numbers[i] -= 55;
-    else
-      numbers[i] -= 48;
+    if (numbers[i] == 32) break;
+    
+    if (numbers[i] > 64) numbers[i] -= 55;
+    else numbers[i] -= 48;
     length++;
   }
-
   scanf("%d", &n);
 
   //계산
