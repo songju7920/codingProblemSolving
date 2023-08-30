67311195887454337ca7241bdc6f46b65efbafa0
오송주 <99471963+songju7920@users.noreply.github.com>
2023-08-30 14:12:07 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,17 +1,15 @@
+//MBTI 데이터
 const MBTI = {
-       R: 0,
-       T: 0,
-       C: 0,
-       F: 0,
-       J: 0,
-       M: 0,
-       A: 0,
-       N: 0,
+    R: 0, T: 0,
+    C: 0, F: 0,
+    J: 0, M: 0,
+    A: 0, N: 0,
 }
     
 function solution(survey, choices) {
     var answer = '';
     
+    //MBTI 데이터 분류
     choices.forEach((choice, idx) => {
         if(4 > choice) {
             MBTI[survey[idx].split("")[0]] += 4 - choice;
@@ -20,6 +18,7 @@ function solution(survey, choices) {
         }
     })
     
+    //MBTI 결정
     answer += MBTI.R < MBTI.T ? "T" : "R"
     answer += MBTI.C < MBTI.F ? "F" : "C"
     answer += MBTI.J < MBTI.M ? "M" : "J"
