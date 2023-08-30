ffaee6395ee6d92c7783fe88953edee6dcf33163
오송주 <99471963+songju7920@users.noreply.github.com>
2023-08-30 14:03:51 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.59 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,25 +1,29 @@
-function solution(survey, choices) {
-    let answer = "";
-    const MBTI = {
-        'R' : 0, 'T' : 0,
-        'C' : 0, 'F' : 0,
-        'J' : 0, 'M' : 0,
-        'A' : 0, 'N' : 0
-    }
+const MBTI = {
+       R: 0,
+       T: 0,
+       C: 0,
+       F: 0,
+       J: 0,
+       M: 0,
+       A: 0,
+       N: 0,
+}
     
-    //MBTI 데이터 수집
-    survey.forEach((surveyType,idx)=>{
-        const [front,back] = surveyType.split('')
-
-        if(choices[idx] < 4) MBTI[front] += Math.abs(choices[idx]-4)
-        else if(choices[idx] > 4) MBTI[back] += Math.abs(choices[idx]-4)
+function solution(survey, choices) {
+    var answer = '';
+    
+    choices.forEach((choice, idx) => {
+        if(4 > choice) {
+            MBTI[survey[idx].split("")[0]] += 4 - choice;
+        } else if(4 < choice) {
+            MBTI[survey[idx].split("")[1]] += choice - 4;
+        }
     })
     
-    //MBTI 결정
-    answer = answer.concat(MBTI['R'] >= MBTI['T'] ? 'R' : 'T')
-    answer = answer.concat(MBTI['C'] >= MBTI['F'] ? 'C' : 'F')
-    answer = answer.concat(MBTI['J'] >= MBTI['M'] ? 'J' : 'M')
-    answer = answer.concat(MBTI['A'] >= MBTI['N'] ? 'A' : 'N')
+    answer += MBTI.R < MBTI.T ? "T" : "R"
+    answer += MBTI.C < MBTI.F ? "F" : "C"
+    answer += MBTI.J < MBTI.M ? "M" : "J"
+    answer += MBTI.A < MBTI.N ? "N" : "A"
     
-    return answer;
+    return answer
 }
