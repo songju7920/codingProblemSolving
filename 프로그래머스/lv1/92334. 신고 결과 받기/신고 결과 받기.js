c56a101c692d04cd5fced479d68c9c7013e894e4
오송주 <99471963+songju7920@users.noreply.github.com>
2023-09-21 09:13:22 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -0,0 +1,25 @@
+function solution(id_list, reports, k) {
+    let answer = new Array(id_list.length).fill(0);
+    let reportedCnts = new Array(id_list.length).fill(0);
+    let bannedList = [];
+    let reportedArrs = [];
+    reports = [...new Set(reports)];
+    
+    reports.map(report => {
+        let accused = report.split(" ")[1];
+        reportedCnts[id_list.indexOf(accused)]++;
+    })
+    
+    reportedCnts.map((reportedCnt, idx) => {
+        if(reportedCnt >= k) bannedList.push(id_list[idx]);
+    })
+    
+    reports.map(report => {
+        let [reporter, reported] = report.split(" ");
+        if(bannedList.indexOf(reported) != -1) {
+            answer[id_list.indexOf(reporter)]++;
+        }
+    })
+    
+    return answer;
+}
