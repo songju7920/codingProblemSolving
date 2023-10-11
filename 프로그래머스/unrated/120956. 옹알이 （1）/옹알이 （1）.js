ebe8630b3a8ecd974d045f5172637ad3d996baf3
오송주 <99471963+songju7920@users.noreply.github.com>
2023-10-11 12:08:32 +0900

    [level unrated] Title: 옹알이 (1), Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,4 +1,4 @@
 function solution(babbling) {
-    let answer = babbling.filter(a => a.replaceAll(/aya|ye|woo|ma/g, "") == "");
+    let answer = babbling.filter(a => !(a.replaceAll(/aya|ye|woo|ma/g, "")));
     return answer.length
 }
