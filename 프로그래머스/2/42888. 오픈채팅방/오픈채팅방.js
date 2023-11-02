31cd5099ad26a87faa2299994ecc7980ef059491
오송주 <99471963+songju7920@users.noreply.github.com>
2023-11-02 19:57:33 +0900

    [level 2] Title: 오픈채팅방, Time: 196.52 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,11 +1,12 @@
 function solution(records) {
+    let length = records.length;
     let hashTable = new Map();
     let result = [];
-    
+
     // 최종 닉네임 도출
     records.map(record => {
         let [action, userID, nickname] = record.split(' ');
-        
+
         if(!hashTable.has(userID)) {
             hashTable.set(userID, nickname);
         } else {
@@ -16,7 +17,7 @@ function solution(records) {
             }
         }
     })
-    
+
     // 메세지 생성
     records.map((record, idx) => {
         let [action, userID, nickname] = record.split(' ');
@@ -27,6 +28,6 @@ function solution(records) {
             result.push(`${nickname}님이 나갔습니다.`);
         }
     })
-    
+
     return result;
 }
