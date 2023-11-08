72ee5c9842be1f8eed9782f8d29accf5a35b1489
오송주 <99471963+songju7920@users.noreply.github.com>
2023-11-08 15:02:36 +0900

    [level unrated] Title: 이어 붙인 수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,9 @@
+function solution(num_list) {
+    let even = '';
+    let odd = '';
+    for(let num of num_list) {
+        if(num % 2 == 0) even += num;
+        else odd += num;
+    }
+    return Number(even) + Number(odd);
+}
